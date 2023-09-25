997a115c93eaefe2703f2c2cf19a41461d4a2a73
Cydeo-Huy <hhuylle@gmail.com>
2023-09-25 16:32:38 -0400

    added TestQueries.sql

---
+++ src/test/resources/TestQueries.sql
@@ -0,0 +1,11 @@
+select * from books order by id desc ;
+
+#FIND NEW BOOK INFO
+select id,name,isbn,year,author,description from books order by id desc;
+
+
+
+#FIND NEW USER INFO
+select * from users;
+select * from users order by id desc ;
+select * from users where is not null order by id desc ;
